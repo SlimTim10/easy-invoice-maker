f472bcef9688eebb9aec16b5c0dde444f36e2589
SlimTim10 <slimtim10@gmail.com>
2024-01-02 12:52:55 -0500

    Fix runtime dependency bundling

--- default.nix
+++ default.nix
@@ -1,13 +1,26 @@
 let
+  inherit (pkgs) haskell wkhtmltopdf;
+  addRuntimeDependency = drv: x: addRuntimeDependencies drv [x];
+  addRuntimeDependencies = drv: xs: haskell.lib.overrideCabal drv (drv: {
+    buildDepends = (drv.buildDepends or []) ++ [ pkgs.makeWrapper ];
+    postInstall = ''
+      ${drv.postInstall or ""}
+      for exe in "$out/bin/"* ; do
+        wrapProgram "$exe" --prefix PATH ":" \
+          ${pkgs.lib.makeBinPath xs}
+      done
+    '';
+  });
+
   config = {
     packageOverrides = pkgs: rec {
       haskellPackages = pkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: rec {
           easy-invoice-maker =
-            haskellPackagesNew.callPackage ./easy-invoice-maker.nix { };
+            haskellPackagesNew.callPackage ./easy-invoice-maker.nix {};
           
           mustache =
-            haskellPackagesNew.callPackage ./nix/mustache.nix { };
+            haskellPackagesNew.callPackage ./nix/mustache.nix {};
         };
       };
     };
@@ -18,4 +31,4 @@ let
 
   pkgs = import <nixpkgs> { inherit config; };
 
-in pkgs.haskellPackages.easy-invoice-maker
+in addRuntimeDependency pkgs.haskellPackages.easy-invoice-maker wkhtmltopdf
--- easy-invoice-maker.nix
+++ easy-invoice-maker.nix
@@ -14,37 +14,23 @@
 , wkhtmltopdf
 }:
 
-let
-  easy-invoice-maker = mkDerivation {
-    pname = "easy-invoice-maker";
-    version = "0.1.0.0";
-    src = ./.;
-    isLibrary = false;
-    isExecutable = true;
-    executableHaskellDepends =[
-      base
-      Decimal
-      mustache
-      safe-money
-      safe-money-aeson
-      smtp-mail
-      mime-mail
-      bytestring
-      time
-      text
-    ];
-    license = lib.licenses.bsd3;
-  };
-
-in
-writeShellApplication {
-  name = "easy-invoice-maker";
-  runtimeInputs = [
-    easy-invoice-maker
-    wkhtmltopdf
+mkDerivation {
+  pname = "easy-invoice-maker";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends =[
+    base
+    Decimal
+    mustache
+    safe-money
+    safe-money-aeson
+    smtp-mail
+    mime-mail
+    bytestring
+    time
+    text
   ];
-
-  text = ''
-    easy-invoice-maker "$@"
-  '';
+  license = lib.licenses.bsd3;
 }
